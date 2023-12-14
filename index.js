a0443a8a8adaa092b7766b97d162c934c6d71a1c
kpzzy <kpzzy@klinelex.com>
2023-12-14 13:14:46 +0900

    colorful 1st edition

---
+++ index.js
@@ -0,0 +1,27 @@
+class colorFulFormat {
+    red (text) {
+        console.log('\x1b[31m%s\x1b[0m', text)
+    }
+
+    green (text) {
+        console.log('\x1b[32m%s\x1b[0m', text)
+    }
+
+    yellow (text) {
+        console.log('\x1b[33m%s\x1b[0m', text)
+    }
+
+    blue (text) {
+        console.log('\x1b[34m%s\x1b[0m', text)
+    }
+
+    magenta (text) {
+        console.log('\x1b[35m%s\x1b[0m', text)
+    }
+
+    cyan (text) {
+        console.log('\x1b[36m%s\x1b[0m', text)
+    }
+}
+
+const ColorFulFormat = new colorFulFormat()
