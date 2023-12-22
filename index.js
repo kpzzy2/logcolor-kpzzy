5f45c5a06becb25cb91f9d3f4401658b875e77e8
kpzzy <kpzzy@klinelex.com>
2023-12-22 09:58:59 +0900

    multiLine Color

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
 class logcolor_kpzzy {
     red (text) {
-        console.log('\x1b[31m%s\x1b[0m', text)
+        const redText = '\x1b[31m%s\x1b[0m';
+
+        const textFormatter = JSON.stringify(text, null, 2);
+
+        for(let line of textFormatter.split('\n')){
+            console.log(redText, line)
+        }
     }
 
     green (text) {
-        console.log('\x1b[32m%s\x1b[0m', text)
+        const greenText = '\x1b[32m%s\x1b[0m';
+
+        const textFormatter = JSON.stringify(text, null, 2);
+
+        for(let line of textFormatter.split('\n')){
+            console.log(greenText, line)
+        }
     }
 
     yellow (text) {
-        console.log('\x1b[33m%s\x1b[0m', text)
+        const yellowText = '\x1b[33m%s\x1b[0m';
+
+        const textFormatter = JSON.stringify(text, null, 2);
+
+        for(let line of textFormatter.split('\n')){
+            console.log(yellowText, line)
+        }
     }
 
     blue (text) {
-        console.log('\x1b[34m%s\x1b[0m', text)
+        const blueText = '\x1b[34m%s\x1b[0m';
+
+        const textFormatter = JSON.stringify(text, null, 2);
+
+        for(let line of textFormatter.split('\n')){
+            console.log(blueText, line)
+        }
     }
 
     magenta (text) {
-        console.log('\x1b[35m%s\x1b[0m', text)
+        const magentaText = '\x1b[35m%s\x1b[0m';
+
+        const textFormatter = JSON.stringify(text, null, 2);
+
+        for(let line of textFormatter.split('\n')){
+            console.log(magentaText, line)
+        }
     }
 
     cyan (text) {
-        console.log('\x1b[36m%s\x1b[0m', text)
+        const cyanText = '\x1b[36m%s\x1b[0m';
+
+        const textFormatter = JSON.stringify(text, null, 2);
+
+        for(let line of textFormatter.split('\n')){
+            console.log(cyanText, line)
+        }
     }
 }
 
